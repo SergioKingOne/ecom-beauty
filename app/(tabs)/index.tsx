f9edb568f905e8ecfa11653295945792c7dc779e
sergiokingone <sergiorobayorr@gmail.com>
2024-06-12 22:51:47 -0500

    layout improvement

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,6 +7,7 @@ import {
   Animated,
   TouchableOpacity,
   Text,
+  StatusBar,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import ProductCard from "@/components/ProductCard";
@@ -46,22 +47,21 @@ const HomeScreen: React.FC = () => {
 
   return (
     <Animated.View style={[styles.container, { opacity: fadeAnim }]}>
+      <StatusBar barStyle="dark-content" />
       <View style={styles.header}>
-        <Text style={styles.logoText}>S'Mode</Text>
-        <View style={styles.headerIcons}>
-          <TouchableOpacity>
-            <Image
-              source={require("@/assets/icons/search.png")}
-              style={styles.icon}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <Image
-              source={require("@/assets/icons/shopping-bag.png")}
-              style={styles.icon}
-            />
-          </TouchableOpacity>
-        </View>
+        <TouchableOpacity>
+          <Image
+            source={require("@/assets/icons/search.png")}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+        <Text style={styles.logoText}>S'MODE</Text>
+        <TouchableOpacity>
+          <Image
+            source={require("@/assets/icons/shopping-bag.png")}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
       </View>
       <View style={styles.categoryContainer}>
         {["All", "Skincare", "Cosmetics", "Fragrance"].map((category) => (
@@ -84,6 +84,43 @@ const HomeScreen: React.FC = () => {
         )}
         numColumns={1}
         contentContainerStyle={styles.flatListContent}
+        ListHeaderComponent={
+          <>
+            <View style={styles.heroImageContainer}>
+              <Image
+                source={{
+                  uri: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/beauty-salon%2C-hair-salon%2C-beauty-parlor%2C-spa-design-template-b60f0b5ad0515a82992e75212468e144_screen.jpg?ts=1698336658",
+                }}
+                style={styles.heroImage}
+              />
+            </View>
+            <View style={styles.featuredSection}>
+              <Image
+                source={{
+                  uri: "https://img.freepik.com/free-photo/selfcare-products-flowers-arrangement_23-2149249576.jpg?t=st=1718250433~exp=1718254033~hmac=0368cf1ed7d61001bb58993ba0338f7d89b9a5c352f67875c15b2187a5ef7405&w=740",
+                }}
+                style={styles.featuredImage}
+              />
+              <Image
+                source={{
+                  uri: "https://img.freepik.com/free-photo/3d-rendering-personal-care-products-fondant-pink_23-2151053857.jpg?t=st=1718250495~exp=1718254095~hmac=cbdf5cd9ed7b80146d5dbf401478df1ba7816101a18c1eb00211ffebb4acf9c6&w=1380",
+                }}
+                style={styles.featuredImage}
+              />
+              <Image
+                source={{
+                  uri: "https://img.freepik.com/free-photo/3d-rendering-personal-care-products-fondant-pink_23-2151053829.jpg?t=st=1718250592~exp=1718254192~hmac=b0e71aa6a5b5d18ddf2997d75a3beea8980b9497001596340a0a4aac27bfdacf&w=740",
+                }}
+                style={styles.featuredImage}
+              />
+            </View>
+            <Text style={styles.subTitle}>SKINCARE COMBO</Text>
+            <Text style={styles.description}>
+              Explore an unrivaled selection of makeup, skincare, hair,
+              fragrance & more from classic & emerging brands.
+            </Text>
+          </>
+        }
       />
     </Animated.View>
   );
@@ -93,6 +130,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fdfbfb",
+    paddingTop: 20, // Added padding to the top
   },
   header: {
     flexDirection: "row",
@@ -140,6 +178,40 @@ const styles = StyleSheet.create({
   flatListContent: {
     paddingHorizontal: 16,
     paddingBottom: 100,
+  },
+  heroImageContainer: {
+    padding: 16,
+    alignItems: "center",
+  },
+  heroImage: {
+    width: "100%",
+    height: 200,
+    borderRadius: 10,
+  },
+  featuredSection: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 16,
+    marginTop: 16,
+  },
+  featuredImage: {
+    width: "32%",
+    height: 150,
+    borderRadius: 10,
+  },
+  subTitle: {
+    fontSize: 20,
+    fontFamily: "Glorious",
+    color: "#131313",
+    paddingHorizontal: 16,
+    paddingTop: 16,
+  },
+  description: {
+    fontSize: 16,
+    color: "#818189",
+    paddingHorizontal: 16,
+    paddingTop: 8,
+    lineHeight: 22,
   },
   footer: {
     flexDirection: "row",
