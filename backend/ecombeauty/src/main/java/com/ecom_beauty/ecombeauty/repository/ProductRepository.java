85364ebff66984b13780231bf0a9bfc4c4e411f3
DESKTOP-GJ24RS2\Diego <diegomendez01soft@gmail.com>
2024-06-28 11:18:21 -0500

    Users EndPoint start to implement JWT auth

--- backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/repository/ProductRepository.java
+++ backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/repository/ProductRepository.java
@@ -1,8 +1,10 @@
 package com.ecom_beauty.ecombeauty.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.ecom_beauty.ecombeauty.models.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer>
 {
 	
