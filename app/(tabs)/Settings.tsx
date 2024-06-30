8d001d34ecea6ff0cc224d3da2841d1a5051afcf
sergiokingone <sergiorobayorr@gmail.com>
2024-06-29 19:57:54 -0500

    switches

--- app/(tabs)/Settings.tsx
+++ app/(tabs)/Settings.tsx
@@ -17,29 +17,41 @@ const Settings = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.sectionTitle}>Settings</Text>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.subtitle}>Personal Information</Text>
+      </View>
       <View style={styles.inputContainer}>
-        <Text style={styles.inputLabel}>Full name</Text>
         <TextInput style={styles.input} placeholder="Full name" />
       </View>
       <View style={styles.inputContainer}>
-        <Text style={styles.inputLabel}>Date of Birth</Text>
         <TextInput style={styles.input} placeholder="12/12/1989" />
       </View>
-      <View style={styles.passwordContainer}>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.subtitle}>Password</Text>
+        <TouchableOpacity>
+          <Text style={styles.changeButtonText}>Change</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={[styles.sectionContainer, { marginBottom: 60 }]}>
         <TextInput
           style={[styles.input, styles.passwordInput]}
           placeholder="Password"
           secureTextEntry
         />
-        <TouchableOpacity style={styles.changeButton}>
-          <Text style={styles.changeButtonText}>Change</Text>
-        </TouchableOpacity>
       </View>
-      <View style={styles.switchContainer}>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.subtitle}>Notifications</Text>
+      </View>
+      <View style={[styles.switchContainer, { marginTop: 20 }]}>
         <Text style={styles.switchLabel}>Sales</Text>
         <Switch
           value={isSalesEnabled}
           onValueChange={(value) => setIsSalesEnabled(value)}
+          thumbColor={isSalesEnabled ? "#f29c1d" : "#fdfbfb"}
+          trackColor={{
+            false: "rgba(243, 176, 51, 0.2)",
+            true: "rgba(243, 176, 51, 0.5)",
+          }}
         />
       </View>
       <View style={styles.switchContainer}>
@@ -47,6 +59,11 @@ const Settings = () => {
         <Switch
           value={isNewArrivalsEnabled}
           onValueChange={(value) => setIsNewArrivalsEnabled(value)}
+          thumbColor={isNewArrivalsEnabled ? "#f29c1d" : "#fdfbfb"}
+          trackColor={{
+            false: "rgba(243, 176, 51, 0.2)",
+            true: "rgba(243, 176, 51, 0.5)",
+          }}
         />
       </View>
       <View style={styles.switchContainer}>
@@ -54,6 +71,11 @@ const Settings = () => {
         <Switch
           value={isDeliveryStatusChangesEnabled}
           onValueChange={(value) => setIsDeliveryStatusChangesEnabled(value)}
+          thumbColor={isDeliveryStatusChangesEnabled ? "#f29c1d" : "#fdfbfb"}
+          trackColor={{
+            false: "rgba(243, 176, 51, 0.2)",
+            true: "rgba(243, 176, 51, 0.5)",
+          }}
         />
       </View>
     </View>
@@ -77,6 +99,13 @@ const styles = StyleSheet.create({
   inputContainer: {
     marginBottom: 20,
   },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    fontFamily: "Glorious",
+    color: "#131313",
+    includeFontPadding: false,
+  },
   inputLabel: {
     fontSize: 16,
     fontWeight: "bold",
@@ -85,17 +114,27 @@ const styles = StyleSheet.create({
     color: "#131313",
   },
   input: {
-    backgroundColor: "#fff",
-    padding: 15,
-    borderRadius: 8,
-    borderWidth: 1,
-    borderColor: "#818189",
+    backgroundColor: "white",
+    paddingHorizontal: 12,
+    paddingVertical: 18,
+    marginBottom: 10,
+    fontSize: 18,
+    borderRadius: 3,
+    width: "100%",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.18,
+    shadowRadius: 1.5,
+    elevation: 2,
   },
-  passwordContainer: {
+  sectionContainer: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    marginBottom: 20,
+    marginBottom: 10,
   },
   passwordInput: {
     flex: 1,
@@ -108,8 +147,9 @@ const styles = StyleSheet.create({
     borderRadius: 8,
   },
   changeButtonText: {
-    color: "#fff",
-    fontFamily: "Glorious",
+    color: "#818189",
+    fontSize: 16,
+    includeFontPadding: false,
   },
   switchContainer: {
     flexDirection: "row",
