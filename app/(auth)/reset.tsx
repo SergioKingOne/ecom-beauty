c81bd3b5917cc2bad2c400920ae9a3899069bdb7
sergiokingone <sergiorobayorr@gmail.com>
2024-07-12 21:38:35 -0500

    reset password

---
+++ app/(auth)/reset.tsx
@@ -0,0 +1,218 @@
+import React, { useState } from "react";
+import {
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  GestureResponderEvent,
+} from "react-native";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+import { ThemedView } from "@/components/ThemedView";
+import { ThemedIcon } from "@/components/ThemedIcon";
+import { ThemedText } from "@/components/ThemedText";
+import { useNavigation } from "@react-navigation/native";
+import { useSignIn } from "@clerk/clerk-expo";
+
+export default function ForgotPassword() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [code, setCode] = useState("");
+  const [successfulCreation, setSuccessfulCreation] = useState(false);
+  const [secondFactor, setSecondFactor] = useState(false);
+  const [error, setError] = useState("");
+  const navigation = useNavigation();
+
+  const { isLoaded, signIn, setActive } = useSignIn();
+
+  if (!isLoaded) {
+    return null;
+  }
+
+  async function create(e: GestureResponderEvent) {
+    e.preventDefault();
+    await signIn
+      ?.create({
+        strategy: "reset_password_email_code",
+        identifier: email,
+      })
+      .then((_) => {
+        setSuccessfulCreation(true);
+        setError("");
+      })
+      .catch((err) => {
+        console.error("error", err.errors[0].longMessage);
+        setError(err.errors[0].longMessage);
+      });
+  }
+
+  async function reset(e: GestureResponderEvent) {
+    await signIn
+      ?.attemptFirstFactor({
+        strategy: "reset_password_email_code",
+        code,
+        password,
+      })
+      .then((result) => {
+        if (result.status === "needs_second_factor") {
+          setSecondFactor(true);
+          setError("");
+        } else if (result.status === "complete") {
+          setActive({ session: result.createdSessionId });
+          setError("");
+        } else {
+          console.log(result);
+        }
+      })
+      .catch((err) => {
+        console.error("error", err.errors[0].longMessage);
+        setError(err.errors[0].longMessage);
+      });
+  }
+
+  const [fontsLoaded] = useFonts({
+    Glorious: require("@/assets/fonts/GLORIOUS.otf"),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+  return (
+    <ThemedView style={styles.container}>
+      <TouchableOpacity
+        style={styles.backButton}
+        onPress={() => navigation.goBack()}
+      >
+        <ThemedIcon name="chevron-back" size={32} />
+      </TouchableOpacity>
+
+      <ThemedText style={styles.title}>Forgot password</ThemedText>
+      {!successfulCreation ? (
+        <>
+          <ThemedText>
+            Please, enter your email address. You will receive a link to create
+            a new password
+          </ThemedText>
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            placeholderTextColor="#818189"
+            value={email}
+            onChangeText={setEmail}
+          />
+          <TouchableOpacity style={styles.signupButton} onPress={create}>
+            <Text style={styles.signupButtonText}>SEND EMAIL</Text>
+          </TouchableOpacity>
+        </>
+      ) : (
+        <>
+          <ThemedText>
+            Please, enter the code you received and your new password
+          </ThemedText>
+          <TextInput
+            style={styles.input}
+            placeholder="Code"
+            placeholderTextColor="#818189"
+            value={code}
+            onChangeText={setCode}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            secureTextEntry
+            placeholderTextColor="#818189"
+            value={password}
+            onChangeText={setPassword}
+          />
+          <TouchableOpacity style={styles.signupButton} onPress={reset}>
+            <Text style={styles.signupButtonText}>RESET PASSWORD</Text>
+          </TouchableOpacity>
+        </>
+      )}
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    paddingTop: 48,
+  },
+  backButton: {
+    marginBottom: 24,
+  },
+  title: {
+    fontFamily: "Glorious",
+    fontSize: 38,
+    marginBottom: 80,
+  },
+  input: {
+    backgroundColor: "white",
+    padding: 12,
+    paddingVertical: 18,
+    marginBottom: 16,
+    marginTop: 18,
+    fontSize: 19,
+    borderRadius: 3,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.18,
+    shadowRadius: 1.5,
+    elevation: 2,
+  },
+  signupButton: {
+    backgroundColor: "#f29c1d",
+    paddingVertical: 16,
+    marginTop: 24,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+  },
+  signupButtonText: {
+    color: "#fff",
+    fontSize: 21,
+    fontWeight: "bold",
+  },
+  socialContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    marginBottom: 36,
+  },
+  socialText: {
+    color: "#818189",
+    marginTop: 12,
+    fontSize: 16,
+  },
+  socialButtons: {
+    flexDirection: "row",
+    marginTop: 24,
+  },
+  socialButton: {
+    marginHorizontal: 24,
+    padding: 6,
+  },
+  socialIcon: {
+    width: 48,
+    height: 48,
+  },
+  signinContainer: {
+    marginTop: 12,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+  signinText: {
+    color: "#818189",
+    fontSize: 16,
+  },
+  signinLink: {
+    color: "#f29c1d",
+  },
+});
