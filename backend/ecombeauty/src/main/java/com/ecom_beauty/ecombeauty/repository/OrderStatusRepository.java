3dd4a99362a1799d32eb684022ee4c304a2aa8d1
Sergio <sergiorobayorr@gmail.com>
2024-08-07 17:44:22 -0500

    order status repo

--- backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/repository/OrderStatusRepository.java
+++ backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/repository/OrderStatusRepository.java
@@ -1,5 +1,14 @@
 package com.ecom_beauty.ecombeauty.repository;
 
-public class OrderStatusRepository {
-    
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ecom_beauty.ecombeauty.models.OrderStatus;
+
+@Repository
+public interface OrderStatusRepository extends JpaRepository<OrderStatus, Integer> {
+    Optional<OrderStatus> findByName(String name);
+    boolean existsByName(String name);
 }
