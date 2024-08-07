3f6a3c836f9afbe0b52e24563c85eb940850918e
Sergio <sergiorobayorr@gmail.com>
2024-08-07 17:45:35 -0500

    product repo

--- backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/repository/ProductRepository.java
+++ backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/repository/ProductRepository.java
@@ -1,11 +1,22 @@
 package com.ecom_beauty.ecombeauty.repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import com.ecom_beauty.ecombeauty.models.Product;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Integer>
-{
-	
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    List<Product> findByCategory_Id(Integer categoryId);
+    List<Product> findByNameContainingIgnoreCase(String name);
+    List<Product> findByPriceBetween(BigDecimal minPrice, BigDecimal maxPrice);
+    List<Product> findByRatingGreaterThanEqual(BigDecimal rating);
+    List<Product> findByStockGreaterThan(Integer stock);
+
+    @Query("SELECT p FROM Product p WHERE p.name LIKE %:keyword% OR p.description LIKE %:keyword%")
+    List<Product> searchProducts(String keyword);
 }
