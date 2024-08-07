ed6492cbf1e19f03783c0b2e1ff5413044d50ac4
Sergio <sergiorobayorr@gmail.com>
2024-08-07 17:43:40 -0500

    order repo

--- backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/repository/OrderRepository.java
+++ backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/repository/OrderRepository.java
@@ -1,5 +1,18 @@
 package com.ecom_beauty.ecombeauty.repository;
 
-public class OrderRepository {
-    
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ecom_beauty.ecombeauty.models.Order;
+import com.ecom_beauty.ecombeauty.models.OrderStatus;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+    List<Order> findByUserId(Integer userId);
+    List<Order> findByStatus(OrderStatus status);
+    List<Order> findByDateBetween(LocalDateTime startDate, LocalDateTime endDate);
+    List<Order> findByUserIdAndStatus(Integer userId, OrderStatus status);
 }
