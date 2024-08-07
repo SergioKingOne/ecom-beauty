8d4451c03e26faf0605ef281c9f5cb5ff861ca29
Sergio <sergiorobayorr@gmail.com>
2024-08-06 22:36:50 -0500

    order model

---
+++ backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/models/Order.java
@@ -0,0 +1,47 @@
+package com.ecom_beauty.ecombeauty.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "status_id")
+    private OrderStatus status;
+
+    @Column
+    private LocalDateTime date;
+
+    @Column(name = "shipping_address", nullable = false, columnDefinition = "TEXT")
+    private String shippingAddress;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        date = LocalDateTime.now();
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
