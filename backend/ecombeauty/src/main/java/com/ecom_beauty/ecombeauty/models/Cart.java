60139c51a5e855f28a9f93b5d0d6c4ee293b31b5
Sergio <sergiorobayorr@gmail.com>
2024-08-06 22:14:48 -0500

    cart

---
+++ backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/models/Cart.java
@@ -0,0 +1,36 @@
+package com.ecom_beauty.ecombeauty.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "carts")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
