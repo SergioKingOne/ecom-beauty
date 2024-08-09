4603536c6e775f25d0ca86be92cc66cc283d6d7b
Sergio <sergiorobayorr@gmail.com>
2024-08-08 22:50:03 -0500

    fixes for jwt

--- backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/models/AuthenticationResponse.java
+++ backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/models/AuthenticationResponse.java
@@ -2,7 +2,7 @@ package com.ecom_beauty.ecombeauty.models;
 
 public class AuthenticationResponse
 {
-	private String token;
+	private final String token;
 	
 	public AuthenticationResponse(String token)
 	{
