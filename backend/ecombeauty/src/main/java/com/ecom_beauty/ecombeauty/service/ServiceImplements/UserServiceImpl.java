4edde60a3ad65597f82caf10a009401f78bf3b77
Sergio <sergiorobayorr@gmail.com>
2024-08-08 20:47:06 -0500

    user service

--- backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/service/ServiceImplements/UserServiceImpl.java
+++ backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/service/ServiceImplements/UserServiceImpl.java
@@ -1,13 +1,14 @@
 package com.ecom_beauty.ecombeauty.service.ServiceImplements;
 
-import com.ecom_beauty.ecombeauty.models.User;
-import com.ecom_beauty.ecombeauty.repository.UserRepository;
-import com.ecom_beauty.ecombeauty.service.UserService;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import com.ecom_beauty.ecombeauty.models.User;
+import com.ecom_beauty.ecombeauty.repository.UserRepository;
+import com.ecom_beauty.ecombeauty.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -16,32 +17,37 @@ public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
 
     @Override
-    public List<User> findAll() {
+    public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     @Override
-    public Optional<User> findById(Integer id) {
+    public Optional<User> getUserById(Integer id) {
         return userRepository.findById(id);
     }
 
     @Override
-    public Optional<User> findByUsername(String username) {
-        return userRepository.findByUsername(username);
-    }
-
-    @Override
-    public Optional<User> findByEmail(String email) {
+    public Optional<User> getUserByEmail(String email) {
         return userRepository.findByEmail(email);
     }
 
     @Override
-    public User save(User user) {
+    public Optional<User> getUserByName(String name) {
+        return userRepository.findByName(name);
+    }
+
+    @Override
+    public User saveUser(User user) {
         return userRepository.save(user);
     }
 
     @Override
-    public void deleteById(Integer id) {
+    public void deleteUser(Integer id) {
         userRepository.deleteById(id);
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
     }
 }
--- backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/service/UserService.java
+++ backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/service/UserService.java
@@ -1,26 +1,16 @@
 package com.ecom_beauty.ecombeauty.service;
 
-import com.ecom_beauty.ecombeauty.models.User;
-
 import java.util.List;
 import java.util.Optional;
 
+import com.ecom_beauty.ecombeauty.models.User;
+
 public interface UserService {
-	// Retrieve all users
-    List<User> findAll();
-
-    // Retrieve a user by ID
-    Optional<User> findById(Integer id);
-
-    // Retrieve a user by username
-    Optional<User> findByUsername(String username);
-
-    // Retrieve a user by email address
-    Optional<User> findByEmail(String email);
-
-    // Save a user to the database
-    User save(User user);
-
-    // Delete a user by ID
-    void deleteById(Integer id);
+    List<User> getAllUsers();
+    Optional<User> getUserById(Integer id);
+    Optional<User> getUserByEmail(String email);
+    Optional<User> getUserByName(String name);
+    User saveUser(User user);
+    void deleteUser(Integer id);
+    boolean existsByEmail(String email);
 }
