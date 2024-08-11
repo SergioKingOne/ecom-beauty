e5f1f1b60bd7f97fa52c0fbaffb8f65206844b5f
DESKTOP-GJ24RS2\Diego <diegomendez01soft@gmail.com>
2024-08-10 19:01:50 -0500

    Add Swagger Document API

--- backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/config/SecurityConfig.java
+++ backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/config/SecurityConfig.java
@@ -43,6 +43,19 @@ public class SecurityConfig
 			.authorizeHttpRequests(authorize -> authorize
 				// Public EndPoints
 				.requestMatchers("/api/v1/health/", "/api/v1/generate-token", "/api/v1/users/signup").permitAll()
+				.requestMatchers(
+	                    // Swagger UI v2
+	                    "/v2/api-docs",
+	                    "/swagger-resources",
+	                    "/swagger-resources/**",
+	                    "/configuration/ui",
+	                    "/configuration/security",
+	                    "/swagger-ui.html",
+	                    "/webjars/**",
+	                    // Swagger UI v3 (OpenAPI)
+	                    "/v3/api-docs/**",
+	                    "/swagger-ui/**"
+	            ).permitAll()
 				.requestMatchers(HttpMethod.OPTIONS).permitAll()
 				.anyRequest().authenticated()
 			)
