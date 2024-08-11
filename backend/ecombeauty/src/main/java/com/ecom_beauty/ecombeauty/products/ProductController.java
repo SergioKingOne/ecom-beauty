07bf1f0b2d7a2cb0f68a8b08574033efa26e0311
DESKTOP-GJ24RS2\Diego <diegomendez01soft@gmail.com>
2024-08-10 19:25:23 -0500

    Begining category controller

--- backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/products/ProductController.java
+++ backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/products/ProductController.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/products")
+@CrossOrigin("*")
 public class ProductController
 {
 	@Autowired
