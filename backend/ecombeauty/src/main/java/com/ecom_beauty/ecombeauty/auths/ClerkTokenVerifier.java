140a3a0cc75f35e0bc35a5ca6cd85b113b0c1207
Sergio <sergiorobayorr@gmail.com>
2024-08-24 15:36:30 -0500

    valid clerk token

--- backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/auths/ClerkTokenVerifier.java
+++ backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/auths/ClerkTokenVerifier.java
@@ -5,15 +5,20 @@ import java.security.interfaces.RSAPublicKey;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 @Component
 public class ClerkTokenVerifier {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClerkTokenVerifier.class);
 
     @Value("${clerk.public-key}")
     private String publicKeyPEM;
@@ -22,27 +27,24 @@ public class ClerkTokenVerifier {
         try {
             RSAPublicKey publicKey = getPublicKey(publicKeyPEM);
             Algorithm algorithm = Algorithm.RSA256(publicKey, null);
-            JWT.require(algorithm)
+            DecodedJWT jwt = JWT.require(algorithm)
                     .build()
                     .verify(token);
             
-            // Additional checks can be performed here if needed
-            // For example, checking the token's expiration
-            
+            LOGGER.info("Token verified successfully");
             return true;
         } catch (JWTVerificationException exception) {
-            // Token is not valid
+            LOGGER.error("Token verification failed: {}", exception.getMessage());
             return false;
         } catch (Exception e) {
-            // Error in public key parsing
+            LOGGER.error("Error in public key parsing or token verification: {}", e.getMessage());
             return false;
         }
     }
 
     private RSAPublicKey getPublicKey(String publicKeyPEM) throws Exception {
-        publicKeyPEM = publicKeyPEM.replace("-----BEGIN PUBLIC KEY-----\\", "")
+        publicKeyPEM = publicKeyPEM.replace("-----BEGIN PUBLIC KEY-----", "")
                 .replace("-----END PUBLIC KEY-----", "")
-                .replace("\\", "")  // Remove the backslashes
                 .replaceAll("\\s", "");
         
         byte[] encoded = Base64.getDecoder().decode(publicKeyPEM);
--- backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/config/JwtAuthenticationFilter.java
+++ backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/config/JwtAuthenticationFilter.java
@@ -6,16 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.ecom_beauty.ecombeauty.users.UserDetailsServiceImpl;
+import com.ecom_beauty.ecombeauty.auths.ClerkTokenVerifier;
 
-import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -24,48 +19,41 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter
 {
-	@Autowired
-	private JwtUtils jwtUtils;
-	
-	@Autowired
-	private UserDetailsServiceImpl userDetailsServiceImpl;
+    @Autowired
+    private ClerkTokenVerifier clerkTokenVerifier;
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
 
-	@Override
-	protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
-	        throws ServletException, IOException {
-	    String requestTokenHeader = request.getHeader("Authorization");
-	    String username = null;
-	    String jwtToken = null;
-		
-	    if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
-	        jwtToken = requestTokenHeader.substring(7);
-	        
-	        try {
-	            username = this.jwtUtils.extractUsername(jwtToken);
-	        } catch (ExpiredJwtException expiredJwtException) {
-	            LOGGER.warn("El token ha expirado");
-	        } catch (Exception e) {
-	            LOGGER.error("Error al procesar el token", e);
-	        }
-	    } else {
-	        LOGGER.warn("Token inválido. No empieza con Bearer String. Encabezado: {}", requestTokenHeader);
-	    }
-		
-	    if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-	        UserDetails userDetails = this.userDetailsServiceImpl.loadUserByUsername(username);
-	        if (this.jwtUtils.validateToken(jwtToken, userDetails)) {
-	            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
-	                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-	            usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-	            
-	            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-	        } else {
-	            LOGGER.warn("El token no es válido");
-	        }
-	    }
-	    filterChain.doFilter(request, response);
-	}
-	
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        String requestTokenHeader = request.getHeader("Authorization");
+        String token = null;
+        
+        if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
+            token = requestTokenHeader.substring(7);
+            
+            try {
+                if (clerkTokenVerifier.verifyToken(token)) {
+                    // Token is valid, you can set up the SecurityContext here if needed
+                    // For now, we'll just log that the token is valid
+                    LOGGER.info("Valid token");
+                    
+                    // If you need to set up an authentication object, you can do it like this:
+                    // UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                    //     null, null, null); // You might want to extract user details from the token
+                    // authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    // SecurityContextHolder.getContext().setAuthentication(authToken);
+                } else {
+                    LOGGER.warn("Invalid token");
+                }
+            } catch (Exception e) {
+                LOGGER.error("Error al procesar el token", e);
+            }
+        } else {
+            LOGGER.warn("Token inválido o no presente. Encabezado: {}", requestTokenHeader);
+        }
+        
+        filterChain.doFilter(request, response);
+    }
 }
