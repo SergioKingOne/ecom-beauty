27eee5011997706a3e0c9362aa5e6b8ab4e2812a
DESKTOP-GJ24RS2\Diego <diegomendez01soft@gmail.com>
2024-08-25 21:33:31 -0500

    Remove backend folder after migration to a separate repository

--- backend/ecombeauty/src/main/java/com/ecom_beauty/ecombeauty/carts/CartService.java
+++
@@ -1,9 +0,0 @@
-package com.ecom_beauty.ecombeauty.carts;
-
-import java.util.Optional;
-
-public interface CartService {
-    Optional<Cart> getCartByUserId(Integer userId);
-    void deleteCartByUserId(Integer userId);
-    Cart saveCart(Cart cart);
-}
