fb7206d4f9e1a4f50351a262a914a8647246ec22
Sergio <sergiorobayorr@gmail.com>
2024-08-30 22:09:23 -0500

    small fix

--- app/(main)/(tabs)/(favorites)/favorites.tsx
+++ app/(main)/(tabs)/(favorites)/favorites.tsx
@@ -155,7 +155,7 @@ const ProductCard: React.FC<{ product: Product; style?: any }> = ({
         <Text style={styles.ratingCount}>({product.rating})</Text>
       </View>
       <View>
-        <Text style={styles.productBrand}>{product.category}</Text>
+        <Text style={styles.productBrand}>{product.category.name}</Text>
         <ThemedText style={styles.productName}>{product.name}</ThemedText>
         <View style={styles.priceContainer}>
           <ThemedText
